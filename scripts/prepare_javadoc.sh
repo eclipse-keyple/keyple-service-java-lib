dc23b66ada5ad98aa1099846871fce6a60fe0a91
Andrei Cristea <andrei.cristea019@gmail.com>
2021-09-21 14:59:36 +0200

    ci: fix javadoc publication process

--- scripts/prepare_javadoc.sh
+++ scripts/prepare_javadoc.sh
@@ -37,7 +37,7 @@ cp -rf ../src/main/uml/api_*.svg $version/
 echo "Update versions list..."
 echo "| Version | Documents |" > list_versions.md
 echo "|:---:|---|" >> list_versions.md
-for directory in `ls -rd */ | cut -f1 -d'/'`
+for directory in `ls -rd [0-9]*/ | cut -f1 -d'/'`
 do
   diagrams=""
   for diagram in `ls $directory/api_*.svg | cut -f2 -d'/'`
